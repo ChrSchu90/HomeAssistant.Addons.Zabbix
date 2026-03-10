8f1736759142d53b63127ac3165fb1b3c96c8656
ChrSchu <chr.schu@outlook.com>
2026-03-10 20:15:08 +0100

    Fixed base build image

--- zabbix60-agent2/debug.sh
+++ zabbix60-agent2/debug.sh
@@ -5,7 +5,7 @@ DOCKER_FILE="Dockerfile"
 IMAGE_NAME="zabbix60-agent2:dev"
 BUILD_VERSION=0.0.1
 BUILD_ARCH=amd64
-BUILD_FROM=ghcr.io/home-assistant/amd64-base-debian:trixie
+BUILD_FROM_BASE=ghcr.io/home-assistant/amd64-base-debian:trixie
 
-docker build --no-cache --pull --rm --build-arg BUILD_VERSION="${BUILD_VERSION}" --build-arg BUILD_ARCH=${BUILD_ARCH} --build-arg BUILD_FROM=${BUILD_FROM} -f ${DOCKER_FILE} -t ${IMAGE_NAME} . && \
+docker build --no-cache --pull --rm --build-arg BUILD_VERSION="${BUILD_VERSION}" --build-arg BUILD_ARCH=${BUILD_ARCH} --build-arg BUILD_FROM_BASE=${BUILD_FROM_BASE} -f ${DOCKER_FILE} -t ${IMAGE_NAME} . && \
     docker run --rm -it -v ./debug:/data ${IMAGE_NAME}
